94a00ef49e92899c585d77c25dcf4b72234e249e
chengchao12138 <chao@chaodeMacBook-Pro.local>
2016-07-13 10:57:37 +0800

    创建project

---
+++ musicPlayer/musicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  musicPlayer
+//
+//  Created by chao on 16/7/13.
+//  Copyright © 2016年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ musicPlayer/musicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  musicPlayer
+//
+//  Created by chao on 16/7/13.
+//  Copyright © 2016年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
